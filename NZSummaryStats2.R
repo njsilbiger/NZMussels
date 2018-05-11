cafec72a8e65e42aa36a6e2ce58a341005f6729d
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2018-05-11 14:15:02 -0700

    updated tables to include SD

--- NZSummaryStats2.R
+++ NZSummaryStats2.R
@@ -10,6 +10,7 @@
 #Calculated LT50 values for NR and BT populations were, respectively, 31.8 and 33.3ºC for Perna, 
 # 31.8 and 33.1ºC for Aulacomya, and 35.2 and 37.9ºC for Mytilus  
 
+library(tidyverse)
 
 ############################# BT historical ####################################
 
@@ -19,6 +20,7 @@ file.names<-basename(list.files(path = path.p, pattern = "txt$", recursive = TRU
 
 # create an empty tibble to fill in
 DaysAboveLT50BT<-tibble('Pern'=rep(NA, length(file.names)),'Aul'=rep(NA, length(file.names)),'Myt'=rep(NA, length(file.names)), 'TideHeight' = seq(50,250,10))
+DaysAboveLT50BT.sd<-tibble('Pern'=rep(NA, length(file.names)),'Aul'=rep(NA, length(file.names)),'Myt'=rep(NA, length(file.names)), 'TideHeight' = seq(50,250,10))
 
 # loop over all the tide height files
 for (i in 1:length(file.names)){
@@ -64,10 +66,24 @@ for (j in 1:length(temps)){
   summarise(meandays = mean(countdays))
   
 }
+# for SD
+# j is index for the 3 LT50s and i is index fot the tide height
+for (j in 1:length(temps)){
+  DaysAboveLT50BT.sd[i,j]<- data %>% 
+    group_by(Date, Year)%>% # group by date and year
+    summarise(counthours = count_hours(Temp,temps[j]))%>% # count how many hours above LT50 per day
+    group_by(Year)%>% # now group by year
+    summarise(countdays = count_6hour(counthours))%>% # now average number of days per year
+    summarise(SDdays = sd(countdays))
+  
+}
 }
 
 # write out the data
+DaysAboveLT50BT<-DaysAboveLT50BT%>%arrange(desc(TideHeight)) # arrange the tide height in descending order
 write.csv(DaysAboveLT50BT, 'SummaryOutput/DaysAboveLT50BT.csv')
+DaysAboveLT50BT.sd<-DaysAboveLT50BT.sd%>%arrange(desc(TideHeight)) # arrange the tide height in descending order
+write.csv(DaysAboveLT50BT.sd, 'SummaryOutput/DaysAboveLT50BT.sd.csv')
 
 ######################## BT Future ######################################
 # find all the filenames
@@ -76,6 +92,7 @@ file.names<-basename(list.files(path = path.p, pattern = "txt$", recursive = TRU
 
 # create an empty tibble to fill in
 DaysAboveLT50BT_future<-tibble('Pern'=rep(NA, length(file.names)),'Aul'=rep(NA, length(file.names)),'Myt'=rep(NA, length(file.names)), 'TideHeight' = seq(50,250,10))
+DaysAboveLT50BT_future.sd<-tibble('Pern'=rep(NA, length(file.names)),'Aul'=rep(NA, length(file.names)),'Myt'=rep(NA, length(file.names)), 'TideHeight' = seq(50,250,10))
 
 for (i in 1:length(file.names)){
   
@@ -107,10 +124,23 @@ for (i in 1:length(file.names)){
       summarise(meandays = mean(countdays))
     
   }
+  #Sd
+  for (j in 1:length(temps)){
+    DaysAboveLT50BT_future.sd[i,j]<- data %>% 
+      group_by(Date, Year)%>% # group by date and year
+      summarise(counthours = count_hours(Temp,temps[j]))%>% # count how many hours above LT50 per day
+      group_by(Year)%>% # now group by year
+      summarise(countdays = count_6hour(counthours))%>% # now average number of days per year
+      summarise(SDdays = sd(countdays))
+    
+  }
 }
 
 # write out the data
+DaysAboveLT50BT_future<-DaysAboveLT50BT_future%>%arrange(desc(TideHeight)) # arrange the tide height in descending order
 write.csv(DaysAboveLT50BT_future, 'SummaryOutput/DaysAboveLT50BT_future.csv')
+DaysAboveLT50BT_future.sd<-DaysAboveLT50BT_future.sd%>%arrange(desc(TideHeight)) # arrange the tide height in descending order
+write.csv(DaysAboveLT50BT_future.sd, 'SummaryOutput/DaysAboveLT50BT_futuresd.csv')
 
 ############################# NR historical ####################################
 
@@ -120,6 +150,7 @@ file.names<-basename(list.files(path = path.p, pattern = "txt$", recursive = TRU
 
 # create an empty tibble to fill in
 DaysAboveLT50NR<-tibble('Pern'=rep(NA, length(file.names)),'Aul'=rep(NA, length(file.names)),'Myt'=rep(NA, length(file.names)), 'TideHeight' = seq(50,250,10))
+DaysAboveLT50NR.sd<-tibble('Pern'=rep(NA, length(file.names)),'Aul'=rep(NA, length(file.names)),'Myt'=rep(NA, length(file.names)), 'TideHeight' = seq(50,250,10))
 
 for (i in 1:length(file.names)){
   
@@ -153,10 +184,27 @@ for (i in 1:length(file.names)){
       summarise(meandays = mean(countdays))
     
   }
+
+
+### SD#####
+for (j in 1:length(temps)){
+  DaysAboveLT50NR.sd[i,j]<- data %>% 
+    group_by(Date, Year)%>% # group by date and year
+    summarise(counthours = count_hours(Temp,temps[j]))%>% # count how many hours above LT50 per day
+    group_by(Year)%>% # now group by year
+    summarise(countdays = count_6hour(counthours))%>% # now average number of days per year
+    summarise(SDdays = sd(countdays))
+  
 }
+}
+# write out the data
+DaysAboveLT50NR<-DaysAboveLT50NR%>%arrange(desc(TideHeight)) # arrange the tide height in descending order
+write.csv(DaysAboveLT50NR, 'SummaryOutput/DaysAboveLT50NR.csv')
 
 # write out the data
-write.csv(DaysAboveLT50NR, 'SummaryOutput/DaysAboveLT50NR.csv')
+DaysAboveLT50NR.sd<-DaysAboveLT50NR.sd%>%arrange(desc(TideHeight)) # arrange the tide height in descending order
+write.csv(DaysAboveLT50NR.sd, 'SummaryOutput/DaysAboveLT50NRsd.csv')
+
 
 ######################## NR Future ######################################
 # find all the filenames
@@ -165,6 +213,7 @@ file.names<-basename(list.files(path = path.p, pattern = "txt$", recursive = TRU
 
 # create an empty tibble to fill in
 DaysAboveLT50NR_future<-tibble('Pern'=rep(NA, length(file.names)),'Aul'=rep(NA, length(file.names)),'Myt'=rep(NA, length(file.names)), 'TideHeight' = seq(50,250,10))
+DaysAboveLT50NR_future.sd<-tibble('Pern'=rep(NA, length(file.names)),'Aul'=rep(NA, length(file.names)),'Myt'=rep(NA, length(file.names)), 'TideHeight' = seq(50,250,10))
 
 for (i in 1:length(file.names)){
   
@@ -196,7 +245,23 @@ for (i in 1:length(file.names)){
       summarise(meandays = mean(countdays))
     
   }
+  #sd
+  for (j in 1:length(temps)){
+    DaysAboveLT50NR_future.sd[i,j]<- data %>% 
+      group_by(Date, Year)%>% # group by date and year
+      summarise(counthours = count_hours(Temp,temps[j]))%>% # count how many hours above LT50 per day
+      group_by(Year)%>% # now group by year
+      summarise(countdays = count_6hour(counthours))%>% # now average number of days per year
+      summarise(SDdays = sd(countdays))
+    
+  }
 }
 
 # write out the data
+DaysAboveLT50NR_future<-DaysAboveLT50NR_future%>%arrange(desc(TideHeight)) # arrange the tide height in descending order
 write.csv(DaysAboveLT50NR_future, 'SummaryOutput/DaysAboveLT50NR_future.csv')
+
+DaysAboveLT50NR_future.sd<-DaysAboveLT50NR_future.sd%>%arrange(desc(TideHeight)) # arrange the tide height in descending order
+write.csv(DaysAboveLT50NR_future.sd, 'SummaryOutput/DaysAboveLT50NR_futuresd.csv')
+
+
